f10945a7df24b88728e7b015894259356dbbd8e0
guocq <guocqmail@163.com>
2017-09-09 16:23:10 +0800

    原来的开源工程改造

---
+++ website/views.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response, RequestContext
+
+
+@login_required
+def Home(request):
+    return render_to_response('home.html', locals(), RequestContext(request))
+
+
+def About(request):
+    return render_to_response('about.html', locals(), RequestContext(request))
